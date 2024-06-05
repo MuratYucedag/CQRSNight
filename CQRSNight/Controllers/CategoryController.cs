4bf459590cb35536a46fe96fb80a183d2e113ba3
MuratYucedag <yucedagmurat23@gmail.com>
2024-06-05 21:16:36 +0300

    ....

---
+++ CQRSNight/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using CQRSNight.CQRSDesignPattern.Commands.CategoryCommands;
+using CQRSNight.CQRSDesignPattern.Handlers.CategoryHandlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRSNight.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        public CategoryController(GetCategoryQueryHandler getCategoryQueryHandler, CreateCategoryCommandHandler createCategoryCommandHandler)
+        {
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+        }
+
+        public IActionResult CategoryList()
+        {
+            var values = _getCategoryQueryHandler.Handle();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryCommand command)
+        {
+            _createCategoryCommandHandler.Handle(command);
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
