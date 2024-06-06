2e51080691d47fe66f06cbf083cae7ca7435c094
MuratYucedag <yucedagmurat23@gmail.com>
2024-06-06 20:39:07 +0300

    ...

---
+++ CQRSNight/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using CQRSNight.CQRSDesignPattern.Commands.ProductCommands;
+using CQRSNight.CQRSDesignPattern.Handlers.ProductHandlers;
+using CQRSNight.CQRSDesignPattern.Queries.ProductQueries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRSNight.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly GetProductQueryHandler _getProductQueryHandler;
+        private readonly CreateProductCommandHandler _createProductCommandHandler;
+        private readonly RemoveProductCommandHandler _removeProductCommandHandler;
+        private readonly GetProductByIdQueryHandler _getProductByIdQueryHandler;
+        private readonly UpdateProductCommandHandler _updateProductCommandHandler;
+        public ProductController(GetProductQueryHandler getProductQueryHandler, CreateProductCommandHandler createProductCommandHandler, RemoveProductCommandHandler removeProductCommandHandler, GetProductByIdQueryHandler getProductByIdQueryHandler, UpdateProductCommandHandler updateProductCommandHandler)
+        {
+            _getProductQueryHandler = getProductQueryHandler;
+            _createProductCommandHandler = createProductCommandHandler;
+            _removeProductCommandHandler = removeProductCommandHandler;
+            _getProductByIdQueryHandler = getProductByIdQueryHandler;
+            _updateProductCommandHandler = updateProductCommandHandler;
+        }
+        public IActionResult ProductList()
+        {
+            var values = _getProductQueryHandler.Handle();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(CreateProductCommand command)
+        {
+            _createProductCommandHandler.Handle(command);
+            return RedirectToAction("ProductList");
+        }
+
+        public IActionResult RemoveProduct(int id)
+        {
+            _removeProductCommandHandler.Handle(new RemoveProductCommand(id));
+            return RedirectToAction("ProductList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = _getProductByIdQueryHandler.Handle(new GetProductByIdQuery(id));
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(UpdateProductCommand command)
+        {
+            _updateProductCommandHandler.Handle(command);
+            return RedirectToAction("ProductList");
+        }
+    }
+}
